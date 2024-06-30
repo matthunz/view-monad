53fef10406868a39bfa03efaf9990d4f2e7b7ed1
Matt Hunzinger <matthunz2@gmail.com>
2024-06-29 23:22:33 -0400

    Setup state

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,15 @@ import ViewMonad
 
 main :: IO ()
 main =
-  let (_, vdom) = buildHtml (HtmlComponent . pure $ Text "A") mkVirtualDom
+  let (_, vdom) =
+        buildHtml
+          ( HtmlComponent $ do
+              (x, setX) <- useState (0 :: Int)
+
+              setX (x + 1)
+
+              return $ Text (show x)
+          )
+          mkVirtualDom
       mutations = rebuildHtml 0 vdom
    in print mutations
--- src/ViewMonad.hs
+++ src/ViewMonad.hs
@@ -4,6 +4,9 @@ module ViewMonad
   ( Node (..),
     Tree,
     Html (..),
+    Component,
+    useHook,
+    useState,
     VirtualDom,
     mkVirtualDom,
     buildHtml,
@@ -12,8 +15,9 @@ module ViewMonad
 where
 
 import Control.Monad (ap)
-import Data.Dynamic (Dynamic)
+import Data.Dynamic (Dynamic, Typeable, fromDyn, fromDynamic, toDyn)
 import Data.IntMap (IntMap, insert, (!))
+import Data.Maybe (fromMaybe)
 
 data Html = HtmlComponent (Component Html) | Fragment [Html] | Element String [Html] | Text String
   deriving (Show)
@@ -85,6 +89,33 @@ instance Monad Component where
            in (b, idx'', hooks'')
       )
 
+useHook :: (Typeable a) => (Int -> Int -> a) -> Component a
+useHook f =
+  Component
+    ( \i idx hooks ->
+        if idx >= length hooks
+          then let x = f i idx in (x, idx + 1, hooks ++ [toDyn x])
+          else
+            let val = hooks !! idx
+             in (fromMaybe (error "TODO") (fromDynamic val), idx + 1, hooks)
+    )
+
+data State a = State Int Int a
+
+useState :: (Typeable a) => a -> Component (a, a -> Component ())
+useState s =
+  ( \(State i idx s') ->
+      ( s',
+        \new ->
+          Component
+            ( \_ _ hooks ->
+                let x = replaceAt idx (toDyn (State i idx new)) hooks
+                 in ((), idx, x)
+            )
+      )
+  )
+    <$> useHook (\i idx -> State i idx s)
+
 data Mutation = SetText Int String
   deriving (Show)
 
@@ -100,3 +131,9 @@ rebuildHtml i vdom = case _tree vdom ! i of
             _ -> error ""
           _ -> error ""
   _ -> error ""
+
+replaceAt :: Int -> a -> [a] -> [a]
+replaceAt _ _ [] = []
+replaceAt n newVal (x : xs)
+  | n == 0 = newVal : xs
+  | otherwise = x : replaceAt (n - 1) newVal xs
